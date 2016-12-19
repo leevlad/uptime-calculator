2716e91d5051011e860e489ec30fed1565899c5f
Makram Kamaleddine <makram@500px.com>
2016-12-19 16:31:26 -0500

    add tsvector to services for service search

---
+++ db/migrate/20161219212921_add_tsvector_to_services.rb
@@ -0,0 +1,28 @@
+class AddTsvectorToServices < ActiveRecord::Migration
+  def change
+    return unless ActiveRecord::Base.connection.adapter_name == "postgresql"
+    # Create the search vector column
+    add_column :services, :search_vector, 'tsvector'
+
+    # Create the gin index on the search vector
+    execute <<-SQL
+      create index services_search_idx
+      on services
+      using gin(search_vector)
+    SQL
+
+    # Trigger to update the vector column when the services
+    # table is updated.
+    execute <<-SQL
+      drop tigger if exists services_search_vector_update
+      on services;
+      create trigger services_search_vector_update
+      before insert or update
+      on services
+      for each row execute procedure
+      tsvector_update_trigger (search_vector, 'pg_catalog.english', summary, symptoms, resolution, follow_ups, tags, hosts);
+    SQL
+
+    Incident.find_each { |i| i.touch }
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219211708) do
+ActiveRecord::Schema.define(version: 20161219212921) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "incidents", force: :cascade do |t|
     t.datetime "incident_time",       null: false
