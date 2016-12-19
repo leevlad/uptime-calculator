7d155f9f183018a2ebfb09534dfcb5131e6412a4
Makram Kamaleddine <makram@500px.com>
2016-12-19 16:26:58 -0500

    remove search ctrlr

--- app/controllers/search_controller.rb
+++
@@ -1,9 +0,0 @@
-class SearchController < ApplicationController
-  def search
-    if params[:search_term].nil? 
-      return
-    end
-    search_term = params[:search_term]
-    @results = Incident.search(search_term)
-  end
-end
